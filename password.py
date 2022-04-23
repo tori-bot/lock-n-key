c08f0d127ec80149bd0b9f54fa9a9e10a0bef1d4
victoria makena <makenavictoria1@gmail.com>
2022-04-23 12:11:29 +0300

    initiate the User class

---
+++ password.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self,fname,lname,username,email,phone_number):
+        self.fname=fname
+        self.lname=lname
+        self.username=username
+        self.email=email
+        self.phone_number=phone_number
+        
