f48059753cfbf7d61491922d8df1f80e7968192d
victoria makena <makenavictoria1@gmail.com>
2022-04-23 12:30:48 +0300

    define the setup class method

--- test-password.py
+++ test-password.py
@@ -2,4 +2,16 @@ import unittest
 from password import User
 from password import Credentials
 
+class TestPassword(unittest.TestCase):
 
+    @classmethod
+    def setUp(cls,self):
+        self.newUser=User('james','kamau','jamie','james@kamau.com','0711223344')
+        self.newCredential=Credentials('instagram','jamiek','james@k.com','igjameskamau')
+        print('setup')
+
+    @classmethod
+    def tearDownClass(cls,self):
+        print('teardown')
+
+      
