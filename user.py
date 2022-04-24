eb88cf21cd8752982f3787c0db8736587221f56b
victoria makena <makenavictoria1@gmail.com>
2022-04-24 12:58:56 +0300

    add function to new save user

--- user.py
+++ user.py
@@ -1,10 +1,19 @@
 class User:
-    def __init__(self,fname,lname,username,email,phone_number):
+    users_list=[]
+    def __init__(self,fname,lname,username,password,email,phone_number):
         self.fname=fname
         self.lname=lname
         self.username=username
+        self.password=password
         self.email=email
         self.phone_number=phone_number
+
+    def save_user(self):
+        #save user in user list
+        User.users_list.append(self)
+        
+
+    
         
 
         
