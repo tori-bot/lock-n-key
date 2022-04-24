b25f5b98d95c07c494aa063454d500ecf6dc8eda
victoria makena <makenavictoria1@gmail.com>
2022-04-24 12:14:51 +0300

    iadd setup function

--- test-credentials.py
+++ test-credentials.py
@@ -2,3 +2,11 @@ import unittest
 import pyperclip
 from credentials import Credentials
 
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        #setup method to run before each test case
+        self.new_credential=Credentials('twitter','tori-bot','tori.bot@gmail.com','qwerty')
+        print('setup successful')
+
+        
+        
