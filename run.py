c3282a158f5bf3c21ba07fd83f5c0980d555defc
victoria makena <makenavictoria1@gmail.com>
2022-04-24 14:54:43 +0300

    add functions to check if user exists,add credential and save it

--- run.py
+++ run.py
@@ -3,7 +3,21 @@ from credentials import Credentials
 import random
 import string
 
-def register(fname,lname,username,password,email,phone_number):
+def register_user(fname,lname,username,password,email,phone_number):
     #function to register new user
     new_user=User(fname,lname,username,password,email,phone_number)
     return new_user
+
+def user_exist_login(password):
+    #find user by password so they can login
+    return User.user_exist(password)
+
+def create_account_credential(account,username,email,password):
+    #function to create new credentials object
+    new_credential=Credentials(account,username,email,password)
+    return new_credential
+
+def save_account_credential(credential):
+    #save credential
+    credential.save_credentials(credential)
+
