c4ca32e5bc84d59b74bb85b92b8b94c7bc47225a
victoria makena <makenavictoria1@gmail.com>
2022-04-24 13:15:12 +0300

    add setup and teardown functions

--- test-user.py
+++ test-user.py
@@ -1,18 +1,17 @@
 import unittest
 from user import User
-from credentials import Credentials
 
 class TestPassword(unittest.TestCase):
-
-    @classmethod
-    def setUp(cls,self):
-        self.newUser=User('james','kamau','jamie','james@kamau.com','0711223344')
-        self.newCredential=Credentials('instagram','jamiek','james@k.com','igjameskamau')
+    def setUp(self):
+        #method runs before each test case
+        self.newUser=User('james','kamau','jamie','qwerty','james@kamau.com','0711223344')
         print('setup')
 
-    @classmethod
-    def tearDownClass(cls,self):
+    def tearDown(self):
+        User.users_list=[]
         print('teardown')
+
+    
 
       
 if __name__=='__main__':
