77a484d98bb44164c5ff49786f0e4ccd14f5b042
victoria makena <makenavictoria1@gmail.com>
2022-04-24 12:38:07 +0300

    add test to check if credentials can be found by account name

--- test-credentials.py
+++ test-credentials.py
@@ -6,12 +6,12 @@ class TestCredentials(unittest.TestCase):
     def setUp(self):
         #setup method to run before each test case
         self.new_credential=Credentials('twitter','tori-bot','tori.bot@gmail.com','qwerty')
-        print('setup successful')
+        
 
     def tearDown(self):
         #function to cleanup after each test is run
         Credentials.credentials_list=[]
-        print('teardown successful')
+        
 
     def test_init(self):
         #test if object is initialized properly
@@ -42,7 +42,16 @@ class TestCredentials(unittest.TestCase):
         self.new_credential.delete_credential()
         self.assertEqual(len(Credentials.credentials_list),1)
 
-        
+    def test_find_by_account(self):
+        #test of a credential can be found using account name
+        self.new_credential.save_credential()
+        test_credential=Credentials('instagram','joyous-cat','joy@cat.com','mnbvcxz')
+        test_credential.save_credential()
+
+        found_credential=Credentials.find_by_account('instagram')
+        self.assertEqual(found_credential.email,test_credential.email)
+
+
 
 
 
