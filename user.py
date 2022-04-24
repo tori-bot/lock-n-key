68fe96e4401b49a4ada187bde61468113b7ed6ab
victoria makena <makenavictoria1@gmail.com>
2022-04-24 13:10:45 +0300

    add function to check if user exists in userlist

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+
+
 class User:
     users_list=[]
     def __init__(self,fname,lname,username,password,email,phone_number):
@@ -8,10 +11,19 @@ class User:
         self.email=email
         self.phone_number=phone_number
 
-    def save_user(self):
+    def register_user(self):
         #save user in user list
         User.users_list.append(self)
-        
+
+    @classmethod
+    def user_login(cls,password):
+        #check if user is registered so they can login
+        for user in cls.users_list:
+            if user.password==password:
+                return True
+        return False
+
+
 
     
         
