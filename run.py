7faac824579ef0ef1fe426ce6e8e1fc896312033
victoria makena <makenavictoria1@gmail.com>
2022-04-24 14:37:56 +0300

    add function to register new user

--- run.py
+++ run.py
@@ -3,3 +3,7 @@ from credentials import Credentials
 import random
 import string
 
+def register(fname,lname,username,password,email,phone_number):
+    #function to register new user
+    new_user=User(fname,lname,username,password,email,phone_number)
+    return new_user
