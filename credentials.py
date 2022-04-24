f9b426d379c46e0912498d1dde5e71e58906a13f
victoria makena <makenavictoria1@gmail.com>
2022-04-24 11:39:48 +0300

    add function to save credentials

--- credentials.py
+++ credentials.py
@@ -1,6 +1,12 @@
 class Credentials:
+    #class that generates new instances of credentials
+    credentials_list=[] #empty credentials list
     def __init__(self,account,username,email,password):
         self.account=account
         self.username=username
         self.email=email
         self.password=password
+
+    def save_credential(self):
+        #save credential objects to credentials list
+        Credentials.credentials_list.append(self)
