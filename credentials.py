c4d617bf65bb32b590352a257323aceabc435c98
victoria makena <makenavictoria1@gmail.com>
2022-04-24 11:33:26 +0300

    move credentials class to its own file

---
+++ credentials.py
@@ -0,0 +1,6 @@
+class Credentials:
+    def __init__(self,account,username,email,password):
+        self.account=account
+        self.username=username
+        self.email=email
+        self.password=password
